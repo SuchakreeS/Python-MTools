a42a5290e8f30d2513b639d88b83038e006ab82d
Max <prd.suchakree@gmail.com>
2019-09-10 18:01:19 +0700

    [update] module

--- Image-Detector/main.py
+++ Image-Detector/main.py
@@ -5,32 +5,38 @@ import zmq
 import math
 import sys, socket
 import json
+import base64
 from network import UDPSender
+from network import UDPReceiver
 from imageProcessing import WristbandDetection
 
+MAIN_IP_NUMBER = "127.0.0.1"
+PYTHON_STREAMMING_PORT = 8051
+UNITY_STREAMMING_PORT = 8052
+PYTHON_RESPONSE_PORT = 8053
 
-def cameraCapture():
-    sender = UDPSender("127.0.0.1", 8051)
-    cam = cv.VideoCapture(0)
-    while(True):  
-        _, img = cam.read()
+def Main():
+    receiver = UDPReceiver(MAIN_IP_NUMBER, PYTHON_STREAMMING_PORT)
+    sender = UDPSender(MAIN_IP_NUMBER, PYTHON_RESPONSE_PORT)
+    while(True): 
+        jsonData = receiver.TryToReceive()
+        if not jsonData:
+            print("NOT THING...")
+            continue
+        byteData = Base64Decode(jsonData["ImageData"])
+        img = ReadImageFromBtyes(byteData)
         # Calculation -----------------------------
         data = WristbandDetection(img)
-        # print(data)
+        print(data)
         if data:
             sender.Send(data)
         # -----------------------------------------
-        cv.imshow('main', img)
 
-        if cv.waitKey(1) & 0xFF == ord('q'):
-            break
+def Base64Decode(_string):
+    return base64.b64decode(_string)
 
-def ConfigWaiting():
-    while True:
-        print("")
+def ReadImageFromBtyes(_bytes):
+    return cv.imdecode(np.frombuffer(_bytes, np.uint8), -1)
 
 if __name__ == "__main__":
-    print("Hi")
-    # ConfigWaiting()
-    cameraCapture()
-    
+    Main()
--- Image-Detector/network.py
+++ Image-Detector/network.py
@@ -1,12 +1,28 @@
 import sys, socket
 import json
 
-class UDPSender():
+BUFFER_SIZE = 65535
+
+class UDPSender:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    
+    def Send(self, data):
+        message = json.dumps(data)
+        self.socket.sendto(bytes(message, "utf-8"), (self.ip, self.port))
+
+class UDPReceiver:
     def __init__(self, ip, port):
         self.ip = ip
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    
-    def Send(self, data):
-        message = json.dumps(data)
-        self.sock.sendto(bytes(message, "utf-8"), (self.ip, self.port))
+        self.sock.bind((ip, port))
+
+    def TryToReceive(self):
+        jsonString = self.sock.recvfrom(BUFFER_SIZE)[0].decode('utf-8')
+        data = None
+        if jsonString:
+            data = json.loads(jsonString)
+        return data
